7e9b97752919d09d8da94a7dded2e9bd0f76bc70
Harold Medrano <harold.medrano@tecsup.edu.pe>
2025-08-28 01:45:31 -0500

    feat: eliminar imports innecesarios en las rutas de usuario

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,8 +1,6 @@
 import { Router } from 'express';
 // Usar imports nombrados si se usa 'export const' en el controller
 import { createUser, deleteUser, listUsers, updateUser, updateUserWithImage, getUser, changeUserPassword, adminChangeUserPassword, updateUserProfile, deactivateUser, activateUser } from './user.controller';
-import { authenticateToken } from '../../shared/middleware/auth.middleware';
-import { registerUser } from '../auth/auth.service';
 
 const router = Router();
 
