be3cb5b58ed5e1c751ea2f9c64460f98813a704f
Harold Medrano <harold.medrano@tecsup.edu.pe>
2025-08-29 21:59:46 -0500

    feat: agregar campo cantidad_cliente en CotizacionProducto y OpProducto

---
+++ prisma/migrations/20250830025720_add_cantidad_cliente_to_productos/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "cotizacion_productos" ADD COLUMN     "cantidad_cliente" INTEGER;
+
+-- AlterTable
+ALTER TABLE "productos_orden_proveedor" ADD COLUMN     "cantidad_cliente" INTEGER;
