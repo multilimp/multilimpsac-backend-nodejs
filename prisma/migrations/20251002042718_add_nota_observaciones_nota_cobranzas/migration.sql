20c9f6184a24930e6720ef29c7f910d6f6ef8d6b
Harold Medrano <harold.medrano@tecsup.edu.pe>
2025-10-02 00:26:05 -0500

    feat(ordenCompra): agregar campo cartaAmpliacion en el modelo de OrdenCompra y notas en OrdenProveedor

---
+++ prisma/migrations/20251002042718_add_nota_observaciones_nota_cobranzas/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "ordenes_proveedor" ADD COLUMN     "nota_cobranzas" TEXT,
+ADD COLUMN     "nota_observaciones" TEXT;
---
+++ prisma/migrations/20251002042848_add_carta_ampliacion/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ordenes_compra" ADD COLUMN     "carta_ampliacion" TEXT;
